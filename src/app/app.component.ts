b76c2edd2af9aee663319cf92b34cc2fce675b2c
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2023-06-24 22:23:53 +0700

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,30 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  title = 'quythanhpubs-frontend';
-  private idleTimeout = 5 * 60 * 1000; // 5 minutes in milliseconds
-  private timer: any;
-  visible: boolean = false
+  title = 'QUÁN NHẬU QUY THANH';
+  // private idleTimeout = 5 * 60 * 1000; // 5 minutes in milliseconds
+  // private timer: any;
+  // visible: boolean = false
   constructor() {
-    this.startTimer();
+    // this.startTimer();
   }
-  startTimer(): void {
-    this.timer = setTimeout(() => {
-      this.showAlert();
-    }, this.idleTimeout);
-  }
+  // startTimer(): void {
+  //   this.timer = setTimeout(() => {
+  //     this.showAlert();
+  //   }, this.idleTimeout);
+  // }
 
-  resetTimer(): void {
-    clearTimeout(this.timer);
-    this.startTimer();
-  }
-  showAlert(): void {
-    this.visible = true;
-  }
+  // resetTimer(): void {
+  //   clearTimeout(this.timer);
+  //   this.startTimer();
+  // }
+  // showAlert(): void {
+  //   this.visible = true;
+  // }
 
-  @HostListener('window:mousemove', ['$event'])
-  @HostListener('window:keydown', ['$event'])
-  onUserActivity(event: Event): void {
-    this.resetTimer();
-  }
+  // @HostListener('window:mousemove', ['$event'])
+  // @HostListener('window:keydown', ['$event'])
+  // onUserActivity(event: Event): void {
+  //   this.resetTimer();
+  // }
 }
